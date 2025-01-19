63115faa00b9dd5d314a7b4668b4639f95383ca0
Azhar Ibn Mostafiz <theazharul@gmail.com>
2025-01-19 22:18:07 +0600

    init

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: {
+	nixosConfigurations.nixos-powerhouse = nixpkgs.lib.nixosSystem {
+	system = "x86_64-linux";
+      		modules = [ ./configuration.nix ];
+    	};
+
+  };
+}
