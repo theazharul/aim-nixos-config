9941967f57dcbd8adb35e24d1d241dc42c2f1078
Azhar Ibn Mostafiz <theazharul@gmail.com>
2025-01-23 10:09:54 +0600

    Mikrotik winbox added

--- configuration.nix
+++ configuration.nix
@@ -223,6 +223,9 @@ programs.zsh = {
   };
 };
 
+# Mikrotik
+programs.winbox.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
